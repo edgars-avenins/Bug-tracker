ffe66940c7752b06e093a9168e72aa0520ae9431
Edgars <edgars.avenins@gmail.com>
2020-04-15 14:55:02 +1200

    get more information from db

--- client/components/Details.jsx
+++ client/components/Details.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 class Details extends React.Component {
 
     handleClick = (e) => {
-        console.log(e.target)
+        //console.log(e.target)
         
         
     }
@@ -18,11 +18,18 @@ class Details extends React.Component {
                 <div onClick={this.handleClick}>
                     <h3>{data.projectName}</h3>
                     <h5>{data.projectDescription}</h5>
+                    <p>Project started by {data.firstName} {data.lastName}</p>
+                    <a href={`mailto:${data.email}`}>{data.email}</a>
                     <hr/>
                     <h4>{data.issueName}</h4>
                     <h6>{data.issueDescription}</h6>
                     <hr/>
                     <p>Priority level: {data.priority}</p>
+                    <hr/>
+                    {
+                        data.assigned == true &&
+                        <h6>{data.issueAssignedUserId}</h6>
+                    }
                 </div>
                 :
                 <h3>Choose an Issue first to see more information about it!</h3>
--- database/details.js
+++ database/details.js
@@ -4,23 +4,44 @@ module.exports = {
     getIssueDetails,
 }
 
-function getIssueDetails(issueID, db = connection){
+function getIssueDetails(issueID, db = connection) {
     return db('issues')
         .join('details', 'details.issue_id', 'issues.id')
         .join('projects', 'projects.id', 'issues.project_id')
-        .select('details.id AS detail_id', 
-            'issues.name AS issueName', 
-            'issues.description AS issueDescription', 
+        .join('users', 'users.id', 'issues.user_id')
+        .join('status', 'status.issue_id', 'issues.id')
+        .select('details.id AS detailId',
+            'issues.id AS issueId',
+            'projects.id AS projectId',
+            'projects.user_id AS projectUserId',
+            'users.id AS issueStartedUserId',
+            'status.user_id AS issueAssignedUserId',
+            'issues.name AS issueName',
+            'issues.description AS issueDescription',
             'projects.name AS projectName',
             'projects.description AS projectDescription',
+            'users.first_name AS firstName',
+            'users.last_name AS lastName',
             '*')
-        .where('issue_id', issueID).first()
+        .where('issueId', issueID).first()
         .then(data => {
             delete data.id
+            delete data.user_id
+            delete data.project_id
+            delete data.issue_id
             delete data.name
             delete data.description
+            delete data.hash
+            delete data.first_name
+            delete data.last_name
+
+console.log(data);
 
             return data
         })
+        .catch(err => console.error('Failed at DB getIssueDetails: ', err))
 }
-//details component should display a join between details/issues/project/users
+
+function getIssueUser(){
+    
+}
