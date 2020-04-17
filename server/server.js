9b80f379b85a5bd36b7642de43d18d3e2995feb9
Edgars <edgars.avenins@gmail.com>
2020-04-17 16:57:24 +1200

    add urlencoded

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
+server.use(express.urlencoded({extended: false}))
 
 server.use('/api/v1/projects', projRoute)
 server.use('/api/v1/issues', issueRoute)
