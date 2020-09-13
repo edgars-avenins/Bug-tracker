1c36655c3cb4d6b41319b017717a2fb512513951
Edgars Avenins <edgars.avenins@gmail.com>
2020-09-14 10:49:13 +1200

    remove store from index setup

---
+++ client/store.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+
+import reducers from './reducers'
+
+
+const store = createStore(reducers, compose(
+    applyMiddleware(thunkMiddleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+  ))
+
+export default store
