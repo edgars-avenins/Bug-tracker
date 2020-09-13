96ffe4787c5f1914c3f4c720518ec5117ff858a4
Edgars Avenins <edgars.avenins@gmail.com>
2020-09-14 10:51:11 +1200

    setup base test structure

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import {Provider} from 'react-redux'
+
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import user from '@testing-library/user-event'
 
+import store from '../client/store'
 import App from '../client/components/App'
 
 //wait for week6 for redux testing with this library
 test('App shows...', () => {
-  render(<App store={{}}/>)
+  render(<Provider store={store}><App /></Provider>)
   return screen.debug()
     .then(data => console.log(data))
 })
