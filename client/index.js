24e9d30785700b4e4ac3ad791af361d2ddc28586
Edgars Avenins <edgars.avenins@gmail.com>
2020-09-14 10:49:24 +1200

    refactor store out of index

--- client/index.js
+++ client/index.js
@@ -1,16 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunkMiddleware from 'redux-thunk'
 
-import reducers from './reducers/'
+import store from './store'
 import App from './components/App'
-
-let store = createStore(reducers, compose(
-  applyMiddleware(thunkMiddleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
-))
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
