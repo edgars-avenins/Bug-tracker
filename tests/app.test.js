c26b544b95eb01395b7fbc3074db785f3d7beb28
Edgars Avenins <edgars.avenins@gmail.com>
2020-09-14 14:07:01 +1200

    app rendering nav,access tests

--- tests/app.test.js
+++ tests/app.test.js
@@ -9,8 +9,24 @@ import store from '../client/store'
 import App from '../client/components/App'
 
 //wait for week6 for redux testing with this library
-test('App shows...', () => {
-  render(<Provider store={store}><App /></Provider>)
-  return screen.debug()
-    .then(data => console.log(data))
+describe('App', () => {
+
+  test('Nav has 1 logo, 1 home button and 3 links', () => {
+    render(<Provider store={store}><App /></Provider>)
+    
+    let logo = screen.getAllByRole('logo')
+    let home = screen.getAllByRole('homeButton')
+    let links = screen.getAllByRole('links')
+    expect(logo.length).toBe(1)
+    expect(links.length).toBe(3)
+    expect(home.length).toBe(1)
+  })
+
+  test('Access show auth options', () => {
+    render(<Provider store={store}><App /></Provider>)
+
+    let optionContainer = screen.getAllByRole('authOptions')
+    expect(optionContainer.length).toBe(1)
+  })
 })
+
